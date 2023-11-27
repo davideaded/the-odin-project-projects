c9250a8e958f241a2fe4dc38fe54db1cee45785e
davideaded <david.castrodc033@gmail.com>
2023-11-27 16:28:35 -0300

    Refactoring and player now is prompet if want to play again

--- hangman/lib/hangman.rb
+++ hangman/lib/hangman.rb
@@ -1,49 +1,62 @@
 filename = 'google-10000-english-no-swears.txt'
-abort("Could not open file #{filename}!") unless File.exist?(filename)
+abort("Could not locate file #{filename}!") unless File.exist?(filename)
 
-dictionary = File.open(filename, 'r')
+DICTIONARY = File.open(filename, 'r')
 
-def words_by_length(file, min_size, max_size)
-  possible_words = file.select { |w| w.chomp.length.between?(min_size, max_size) }
+def words_by_length(min_size, max_size)
+  possible_words = DICTIONARY.select { |w| w.chomp.length.between?(min_size, max_size) }
 end
 
-def define_word(file)
-  available_words = words_by_length(file, 5, 12)
-  random = rand(0..available_words.length)
-  available_words[random].chomp
+AVAILABLE_WORDS = words_by_length(5, 12)
+
+def define_word
+  random = rand(0..AVAILABLE_WORDS.length)
+  AVAILABLE_WORDS[random].chomp
 end
 
-def occult_word
+def occult_word(original_word)
   word = ''
-  until word.length == CORRECT_WORD.length do
+  until word.length == original_word.length do
     word << '_'
   end
 
   word
 end
 
-def start_game(word, chances)
+def start_game(chances)
+  correct_word = define_word
+  occulted_word = occult_word(correct_word)
+  guesses = []
+  
+  puts "Enter a letter:"
   loop do
     guess = gets.chomp
     correct_guess = false
+    guesses.push(guess)
 
-    CORRECT_WORD.split("").each_with_index do |l, i|
-      (word[i] = guess) && (correct_guess = true) if l == guess && word[i] == '_'
+    correct_word.split("").each_with_index do |l, i|
+      (occulted_word[i] = guess) && (correct_guess = true) if l == guess && occulted_word[i] == '_'
     end
 
     chances -= 1 if !correct_guess
-    puts "#{word}\t #{chances} chances"
-    break if chances == 0 || !word.include?('_')
+    puts "Used letters: #{guesses}"
+    puts "#{occulted_word}\t #{chances} chances"
+    break if chances == 0 || !occulted_word.include?('_')
   end
+
+  occulted_word.include?('_') ? (puts "You lost! The word was: #{correct_word}") : (puts "You win!")
 end
 
 def init_game
   chances = 5
-  occulted_word = occult_word
+  start_game(chances)
+  response = ''
 
-  puts occulted_word
-  start_game(occulted_word, chances)
+  while response != 'n' do 
+    puts "Play again? (Y/N)"
+    response = gets.chomp.downcase
+    start_game(chances) if response == 'y'
+  end
 end
 
-CORRECT_WORD = define_word(dictionary)
 init_game
