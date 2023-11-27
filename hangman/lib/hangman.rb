2895d68159c2e0a395e7d61a39cac220c02a54f7
davideaded <david.castrodc033@gmail.com>
2023-11-27 10:13:59 -0300

    Added method to hide the word and try to guess

--- hangman/lib/hangman.rb
+++ hangman/lib/hangman.rb
@@ -10,8 +10,41 @@ end
 def define_word(file)
   available_words = words_by_length(file, 5, 12)
   random = rand(0..available_words.length)
-  available_words[random]
+  available_words[random].chomp
 end
+
+def print_guess
+  occult_word = ''
+  until occult_word.length == CORRECT_WORD.length do
+    occult_word << '_'
+  end
+
+  while occult_word.include?('_') do
+    guess = gets.chomp
+    CORRECT_WORD.split("").each_with_index do |l, i|
+      occult_word[i] = guess if l == guess && occult_word[i] == '_'
+    end
+    p occult_word
+  end
+
+  p occult_word
+end
+
+CORRECT_WORD = define_word(dictionary)
+puts CORRECT_WORD
+print_guess
+
+
+
+
+
+
+
+
+
+
+
+
 
 # def write_to_file(dictionary)
 #   words_arr = words_by_length(dictionary, 5, 12)
