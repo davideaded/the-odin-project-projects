5b320a89efa65edec3f56f585f2d5b1d28fd8280
davideaded <david.castrodc033@gmail.com>
2023-11-29 09:51:42 -0300

    Refactoring to implement save current match system

--- hangman/lib/hangman.rb
+++ hangman/lib/hangman.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 class Player
   attr_accessor :guess, :guesses
 
@@ -8,7 +10,7 @@ class Player
   def make_guess
     letter = gets.chomp.downcase
     checked_guess = check_player_guess(letter, self.guesses)
-    self.guesses.push(checked_guess) if checked_guess != nil
+    self.guesses.push(checked_guess) if checked_guess != nil && checked_guess != '0'
     self.guess = checked_guess
   end
 
@@ -33,24 +35,26 @@ class Player
 end
 
 class Dictionary
-  attr_accessor :dictionary, :available_words
+  attr_accessor :correct_word, :occulted_word
   FILENAME = 'google-10000-english-no-swears.txt'
 
   def initialize
     abort("Could not locate file #{FILENAME}!") unless File.exist?(FILENAME)
-    self.dictionary = File.open(FILENAME, 'r')
-    self.available_words = words_by_length
+    @correct_word = define_word
+    @occulted_word = occult_word(@correct_word)
   end
 
   def words_by_length
+    dictionary = File.open(FILENAME, 'r')
     min_size = 5
     max_size = 12
-    possible_words = self.dictionary.select { |w| w.chomp.length.between?(min_size, max_size) }
+    possible_words = dictionary.select { |w| w.chomp.length.between?(min_size, max_size) }
   end
 
   def define_word
-    random = rand(0..self.available_words.length)
-    self.available_words[random].chomp
+    available_words = words_by_length
+    random = rand(0..available_words.length)
+    available_words[random].chomp
   end
 
   def occult_word(original_word)
@@ -65,51 +69,78 @@ end
 class Game
   attr_accessor :player, :dictionary
 
-  def initialize
-    self.player = Player.new
-    self.dictionary = Dictionary.new
+  def initialize(player = Player.new, dictionary = Dictionary.new)
+    @player = player
+    @dictionary = dictionary
   end
 
-  def start_game(correct_word, occulted_word)
-    chances = 5
+  def check_guess(guess)
+    correct_word = dictionary.correct_word
+    occulted_word = dictionary.occulted_word
+
+    correct_word.split("").each_with_index do |l, i|
+      occulted_word[i] = guess if l == guess && occulted_word[i] == '_'
+    end
+
+    occulted_word 
+  end
+
+  def save_game
+    data = { 'player' => @player, 'dictionary' => @dictionary }
+    dump = YAML.dump(data)
+    File.open('savegame.yaml', 'w') { |file| file.write dump }
+    exit(0)
+  end
+
+  def load_game
+    file = File.open('savegame.yaml', 'r')
+    YAML.load(file)
+  end
+
+  def start_game
+    chances = 7
     loop do
-      puts "Enter a letter:"
+      puts "Enter a letter or press '0' to save current game"
       guess = player.make_guess
-
+      save_game if guess == '0'
       next if !guess.is_a?(String)
-      correct_guess = false
+      occulted_word = check_guess(guess)
 
-      correct_word.split("").each_with_index do |l, i|
-        (occulted_word[i] = guess) && (correct_guess = true) if l == guess && occulted_word[i] == '_'
-      end
-
-      chances -= 1 if !correct_guess
+      chances -= 1 if !occulted_word.include?(guess)
       puts "Used letters: #{player.guesses}"
       puts "#{occulted_word}\t #{chances} chances"
       break if chances == 0 || !occulted_word.include?('_')
     end
-    occulted_word.include?('_') ? (puts "You lost! The word was: #{correct_word}") : (puts "You won!")
+    dictionary.occulted_word.include?('_') ? (puts "You lost! The word was: #{dictionary.correct_word}") : (puts "You won!")
   end
 
   def init_game
-    correct_word = dictionary.define_word
-    occulted_word = dictionary.occult_word(correct_word)
     response = ''
 
-    start_game(correct_word, occulted_word)
-
-    while response != 'n' do 
-      puts "Play again? (Y/N)"
-      response = gets.chomp.downcase
-      if response == 'y'
-        correct_word = dictionary.define_word
-        occulted_word = dictionary.occult_word(correct_word)
-        player.clear_guesses
-        start_game(correct_word, occulted_word)
-      end
-    end
+    start_game
+    #
+    # while response != 'n' do 
+    #   puts "Play again? (Y/N)"
+    #   response = gets.chomp.downcase
+    #   if response == 'y'
+    #     correct_word = dictionary.define_word
+    #     occulted_word = dictionary.occult_word(correct_word)
+    #     player.clear_guesses
+    #     start_game(correct_word, occulted_word)
+    #   end
+    # end
   end
 end
 
-game = Game.new
+def load_save
+  saved_data = YAML.safe_load(File.read('savegame.yaml'), permitted_classes: [Player, Dictionary, Game])
+  
+  player_data = saved_data['player']
+  dictionary_data = saved_data['dictionary']
+  return player_data, dictionary_data
+end
+
+# game = Game.new(player, dictionary)
+datas = load_save
+game = Game.new(datas[0], datas[1])
 game.init_game
