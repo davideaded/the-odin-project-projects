1bc12bec076c12367b2f4638d3a6a4ded33c7f37
davideaded <david.castrodc033@gmail.com>
2023-12-10 13:55:56 -0300

    Build tree method refactored and implementation of insert and delete methods

--- b_search_tree/balanced_search_tree.rb
+++ b_search_tree/balanced_search_tree.rb
@@ -8,16 +8,18 @@ class Node
 end
 
 class Tree
+  attr_reader :root
   def initialize(arr)
-    @root = build_tree(arr)
+    @root = build_tree(arr.sort.uniq)
   end
 
-  def build_tree(arr, start = 0, final = arr.length)
-    return nil if start > final
-    mid = (start + final) / 2
+  def build_tree(arr)
+    return nil if arr.empty?
+
+    mid = arr.length / 2
     root = Node.new(arr[mid])
-    root.left = build_tree(arr, start, mid - 1)
-    root.right = build_tree(arr, mid + 1, final)
+    root.left = build_tree(arr[0...mid])
+    root.right = build_tree(arr[(mid + 1)..-1])
     root
   end
 
@@ -26,9 +28,43 @@ class Tree
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
+
+  def insert(node = @root, value)
+    return Node.new(value) if node.nil?
+
+    if value > node.data
+      node.right = insert(node.right, value)
+    elsif value < node.data
+      node.left = insert(node.left, value)
+    end
+    node
+  end
+
+  def delete(node = @root, value)
+    return node if node.nil?
+    if value < node.data
+      node.left = delete(node.left, value)
+    elsif value > node.data
+      node.right = delete(node.right, value)
+    else
+      return node.right if node.left.nil?
+      return node.left if node.right.nil?
+
+      leftmost_node = leftmost_leaf(node.right)
+      node.data = leftmost_node.data
+      node.right = delete(node.right, leftmost_node.data)
+    end
+    node
+  end
+
+  def leftmost_leaf(node)
+    node = node.left until node.left.nil?
+    node
+  end
 end
 
 arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-
 tree = Tree.new(arr)
+tree.delete(4)
+tree.delete(67)
 tree.pretty_print
