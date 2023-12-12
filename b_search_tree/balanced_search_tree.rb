0aaaf1ab9cde12147cefc10541a1d50b8b70272d
davideaded <david.castrodc033@gmail.com>
2023-12-12 10:01:47 -0300

    Preorder method to depth-first traverse the tree

--- b_search_tree/balanced_search_tree.rb
+++ b_search_tree/balanced_search_tree.rb
@@ -94,7 +94,25 @@ class Tree
     queue.push(node.right) unless node.right.nil?
     rec_level_order(queue, values, &block)
   end
+
+  def preorder
+    node = @root
+    values = []
+    stack = [node]
+
+    while !stack.empty?
+      node = stack.pop
+      yield node if block_given?
+      values << node.data
+      stack.push(node.right) if node.right != nil
+      stack.push(node.left) if node.left != nil
+    end
+    values
+  end
 end
 
 arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 tree = Tree.new(arr)
+tree.pretty_print
+p tree.preorder
+tree.preorder {|n| p n.data / 2}
