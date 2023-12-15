410eee654574be22a8bb3083bb6794167bf7a948
davideaded <david.castrodc033@gmail.com>
2023-12-15 16:58:24 -0300

    Refactoring

--- knights_travails/knights_travails.rb
+++ knights_travails/knights_travails.rb
@@ -1,39 +1,50 @@
-POSSIBLE_MOVES = [
-  [1, 2],
-  [2, 1],
-  [2, -1],
-  [1, -2],
-  [-1, -2],
-  [-2, -1],
-  [-2, 1],
-  [-1, 2]
-]
+class Node
+  attr_reader :position, :parent
+  POSSIBLE_MOVES = [
+    [1, 2],
+    [2, 1],
+    [2, -1],
+    [1, -2],
+    [-1, -2],
+    [-2, -1],
+    [-2, 1],
+    [-1, 2]
+  ]
 
-board = Array.new(8) {Array(0..8)}
-
-def available_positions(position)
-  positions = []
-  for i in POSSIBLE_MOVES
-      for j in (0..1)
-        available_moves = (i[j] + position[j]) if i
-        positions << available_moves
-      end
+  def initialize(position, parent)
+    @position = position
+    @parent = parent
   end
-  positions.each_slice(2)
-    .to_a
-    .select { |e| valid?(e) }
-end
 
-def populate_board(board)
-  board.each_with_index do |e, i|
-    e.map!.with_index { |ie, ii| ie = show_available_positions([i, ii]) }
+  def available_positions
+    positions = []
+    for i in POSSIBLE_MOVES
+        for j in (0..1)
+          available_moves = (i[j] + @position[j]) if i
+          positions << available_moves
+        end
+    end
+    positions.each_slice(2)
+      .to_a
+      .select { |e| valid?(e) }
+      .map { |e| Node.new(e, self) }
   end
-  board
-end
 
-def valid?(position)
-  position[0].between?(0, 7) && position[1].between?(0, 7)
-end
+  def valid?(position)
+    position[0].between?(0, 7) && position[1].between?(0, 7)
+  end
 
-populate_board(board)
-p board[1][0]
+  def self.knight_move(start, end_pos)
+    queue = []
+    node = Node.new(start, nil)
+    until node.position == end_pos
+      node.available_positions.each { |e| queue.push(e) }
+      node = queue.shift
+    end
+    until node.parent.nil?
+      p node.position
+      node = node.parent
+    end
+  end
+end
+# Node.knight_move([0,0], [7,3])
