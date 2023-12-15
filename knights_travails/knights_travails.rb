28149e964a6b1433b6cd7c4f2eee26ec3aff161c
davideaded <david.castrodc033@gmail.com>
2023-12-15 10:01:33 -0300

    Refactoring, validation method and adjacency matrix for weighted directed graph with chess knight rules

--- knights_travails/knights_travails.rb
+++ knights_travails/knights_travails.rb
@@ -8,18 +8,32 @@ POSSIBLE_MOVES = [
   [-2, 1],
   [-1, 2]
 ]
-CURRENT_POSITION = [2,2]
 
-def show_available_positions
+board = Array.new(8) {Array(0..8)}
+
+def available_positions(position)
   positions = []
   for i in POSSIBLE_MOVES
-    target_move = i
       for j in (0..1)
-        available_moves = (target_move[j] + CURRENT_POSITION[j]) if target_move
-        positions << available_moves if available_moves.between?(0, 7)
+        available_moves = (i[j] + position[j]) if i
+        positions << available_moves
       end
   end
-  targets.each_slice(2).to_a.uniq
+  positions.each_slice(2)
+    .to_a
+    .select { |e| valid?(e) }
 end
 
-p show_available_positions
+def populate_board(board)
+  board.each_with_index do |e, i|
+    e.map!.with_index { |ie, ii| ie = show_available_positions([i, ii]) }
+  end
+  board
+end
+
+def valid?(position)
+  position[0].between?(0, 7) && position[1].between?(0, 7)
+end
+
+populate_board(board)
+p board[1][0]
